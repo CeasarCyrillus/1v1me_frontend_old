7829e824fdac35ebef382fcfa460cdfd7a1b7585
Ceasar Cyrillus <ceasarcyrillus@businesspartner.roche.com>
2021-02-24 17:16:02 +0100

    Send backend request

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import './App.css';
 
-export const App = () =>
-    <div className="App">
-        <h1>Hello there friend</h1>
-    </div>
+const sendBackendRequest = async () => {
+    const response = await fetch("http://localhost:3001/", {headers: {
+        }});
+    const text = await response.text();
+    console.log(text);
+}
+
+sendBackendRequest();
+export const App = () => {
+    return (
+        <div className="App">
+            <h1>Hello there friend</h1>
+        </div>
+    )
+}
