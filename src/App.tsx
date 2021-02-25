4cc0cd64cf2adb9e8a3fbd29aa6401f16bad42ff
Ceasar Cyrillus <ceasarcyrillus@businesspartner.roche.com>
2021-02-25 18:51:00 +0100

    Cleanup App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Provider, useDispatch, useSelector} from 'react-redux'
-import {RootState, store} from './store';
 
-const sendBackendRequest = async () => {
-    const response = await fetch("http://localhost:3001/", {
-        headers: {}
-    });
-    const text = await response.text();
-    console.log(text);
-}
-
-const connectToWs = () => {
-    const ws = new WebSocket("wss://ws.mynano.ninja/");
-    ws.addEventListener("open", (event) => {
-        console.log(event);
-        ws.send(JSON.stringify({
-            "action": "subscribe",
-            "topic": "confirmation",
-            "ack": true,
-        }))
-    });
-
-    ws.addEventListener("message", (message) => {
-        console.log(message);
-    })
-}
 export const App = () => {
     return (
         <div className="App">
---
+++ src/utils.ts
@@ -0,0 +1,23 @@
+export const sendBackendRequest = async () => {
+    const response = await fetch("http://localhost:3001/", {
+        headers: {}
+    });
+    const text = await response.text();
+    console.log(text);
+}
+
+export const connectToWs = () => {
+    const ws = new WebSocket("wss://ws.mynano.ninja/");
+    ws.addEventListener("open", (event) => {
+        console.log(event);
+        ws.send(JSON.stringify({
+            "action": "subscribe",
+            "topic": "confirmation",
+            "ack": true,
+        }))
+    });
+
+    ws.addEventListener("message", (message) => {
+        console.log(message);
+    })
+}
