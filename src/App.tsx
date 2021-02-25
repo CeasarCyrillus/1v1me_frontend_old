1979c443c922404120be50a3cdebc4c049868fc5
Ceasar Cyrillus <ceasarcyrillus@businesspartner.roche.com>
2021-02-25 19:31:26 +0100

    Create landing page for player 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,18 @@ import './App.css';
 
 export const App = () => {
     return (
-        <div className="App">
-            <h1>Hello there friend</h1>
+        <div className={"app"}>
+            <h1>1v1 me</h1>
+
+            <input className={"addressInput"} data-testid={"address-input"} type={"text"} placeholder={"nano address"} />
+            <br />
+
+            <input className={"betAmountInput"} data-testid={"bet-amount-input"} type={"number"} placeholder={"bet amount"} />
+            <br />
+
+            <button className={"create1v1Button"} data-testid={"create-1v1-button"}>
+                Create 1v1
+            </button>
         </div>
     )
 }
