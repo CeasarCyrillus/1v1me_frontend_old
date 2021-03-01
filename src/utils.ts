eaae50424562122db72316b954849f92604bd9cf
Ceasar Cyrillus <ceasarcyrillus@businesspartner.roche.com>
2021-03-01 21:14:27 +0100

    Update ws

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+export const MY_NANO_ADDRESS = "nano_3x3r177uxmk33hi9wk186dmhhikicbs79h78g8bmci8ghqxc7bqbg6x6a1oa"
 export const sendBackendRequest = async () => {
     const response = await fetch("http://localhost:3001/", {
         headers: {}
@@ -5,14 +6,18 @@ export const sendBackendRequest = async () => {
     const text = await response.text();
 }
 
-export const connectToWs = () => {
-    const ws = new WebSocket("wss://ws.mynano.ninja/");
+export const connectToWs = (address:string) => {
+    const ws = new WebSocket("wss://socket.nanos.cc");
+    console.log("Listening for " + address)
     ws.addEventListener("open", (event) => {
         console.log(event);
         ws.send(JSON.stringify({
             "action": "subscribe",
             "topic": "confirmation",
             "ack": true,
+            "options": {
+                "accounts": [address]
+            }
         }))
     });
 
